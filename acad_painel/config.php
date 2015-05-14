5a59fbe66848aedaebffcd7dd4aa02973b4c7702
Manfrin <djalma@codegear.com>
2015-05-13 22:29:01 -0300

    Site com painel e bd
    O Site está funcionando normal. O painel também.

---
+++ acad_painel/config.php
@@ -0,0 +1,17 @@
+﻿<?php
+	
+    //Diretório do Sistema
+    define("BASEPATH",dirname(__FILE__)."/");
+	define("BASEURL", "http://localhost/Github/prj_engenharia/acad_painel/");
+	define("ADMURL", BASEURL."painel.php");
+	define("CLASSESPATH", "classes/");
+	define("MODULOPATH", "modulos/");
+	define("CSSPATH", "css/");
+	define("JSPATH", "js/");
+	
+	//banco de bados
+	define("BDHOST", "localhost");
+	define("BDUSER","root");
+	define("BDPASS","");
+	define("BDNAME","acadbd");
+?>
